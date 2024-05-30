a1c786c732e33ce201306c49114aa88d99368e34
hnhaoo <hao170504@gmail.com>
2024-05-30 11:28:09 +0700

    fix models

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,11 @@
 from .random_forest import RandomForest
-from .sarimax import Sarimax
-# from .orbit import Orbit
-from .LSTM import MyLSTM
-from .GRU import MyGRU
-from .arima import MyARIMA
-# from .prophet import MyProphet
+from .orbit import Orbit
+from .prophet import MyProphet
 from .xgboost import MyXGboost
 
 
 MODELS = {'random_forest': RandomForest,
-          'sarimax': Sarimax,
-          # 'orbit': Orbit,
-          'lstm': MyLSTM,
-          'gru': MyGRU,
-          'arima': MyARIMA,
-          # 'prophet': MyProphet,
-          'xgboost': MyXGboost
+          'orbit': Orbit,
+          'prophet': MyProphet,
+          'xgboost': MyXGboost,
           }
